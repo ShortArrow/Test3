b7a12de1908f85bd5849146b6ec1dfb6f1806d13
kuroneko <kskkm0228@gmail.com>
2019-04-20 21:04:30 +0900

    I Love My Biz2

--- hello/urls.py
+++ hello/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("kariru", views.kariru, name="kariru"),
     path("kasu", views.kasu, name="kasu"),
+    path("kakunin", views.kakunin, name="kakunin"),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .forms import HelloForm
 
 
 def index(request):
     return render(request, 'hello/index.html')
 
 def kasu(request):
-    return render(request, 'hello/kasu.html')
+    params = {
+            'title': 'Hello',
+            'message': 'your data:',
+            'form': HelloForm()
+        }
+    
+    if (request.method == 'POST'):
+        params['message'] = '名前：' + request.POST['name'] + \
+            '<br>メール：' + request.POST['mail'] + \
+            '<br>年齢：' + request.POST['age']
+        params['form'] = HelloForm(request.POST)
+    return render(request, 'hello/kasu.html', params)
 
 def kariru(request):
     return render(request, 'hello/kariru.html')
+
+def kakunin(request):
+    return render(request, 'hello/kakunin.html')
