6ae0fefffe491bfadfc2580ffb7b626685121dfa
ShortArrow <bamboogeneral@live.jp>
2019-04-21 17:04:26 +0900

    Revert "create crudtest"

--- test3/urls.py
+++ test3/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("hello.urls")),
-    path('crudtest/', include("crudtest.urls")),
+    path('', include("hello.urls")), 
 ]
