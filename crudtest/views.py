2a2e31e565d8d227944f6f29b346b8ad5f760ac3
ShortArrow <bamboogeneral@live.jp>
2019-04-21 15:19:25 +0900

    can view friends

--- crudtest/views.py
+++ crudtest/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from crudtest.models import Friend
 
 
 def index(request):
-    return HttpResponse("Hello Crud!!")
+    data = Friend.objects.all()
+    params = {
+        'title': 'Hello',
+        'message': 'all friends.',
+        'data': data,
+    }
+    return render(request, 'crudtest/index.html', params)
