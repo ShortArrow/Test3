7b33c2ddd807e1915ab2e860efccf330e561d472
watakei <kaoru.2.24@icloud.com>
2019-04-23 21:17:03 +0900

    home

--- home/forms.py
+++ home/forms.py
@@ -1,6 +1,6 @@
 ﻿from django import forms
 
-class HelloForm(forms.Form):
+class homeForm(forms.Form):
     name = forms.CharField(label='name')
     mail = forms.CharField(label='mail')
     age = forms.IntegerField(label='age')
--- home/views.py
+++ home/views.py
@@ -1,28 +1,28 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-from .forms import HelloForm
+from .forms import homeForm
 
 
 def index(request):
-    return render(request, 'hello/index.html')
+    return render(request, 'home/index.html')
 
 def kasu(request):
     params = {
-            'title': 'Hello',
+            'title': 'home',
             'message': 'your data:',
-            'form': HelloForm()
+            'form': homeForm()
         }
     
     if (request.method == 'POST'):
         params['message'] = '名前：' + request.POST['name'] + \
             '<br>メール：' + request.POST['mail'] + \
             '<br>年齢：' + request.POST['age']
-        params['form'] = HelloForm(request.POST)
-    return render(request, 'hello/kasu.html', params)
+        params['form'] = homeForm(request.POST)
+    return render(request, 'home/kasu.html', params)
 
 def kariru(request):
-    return render(request, 'hello/kariru.html')
+    return render(request, 'home/kariru.html')
 
 def kakunin(request):
     name = request.POST['name']
@@ -39,4 +39,4 @@ def kakunin(request):
         'money':money,
         'monkey':monkey,
     }
-    return render(request, 'hello/kakunin.html', params)
+    return render(request, 'home/kakunin.html', params)
