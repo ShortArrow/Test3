6c94217f827c8426ed0adb9447b7cf4bdadfb792
ShortArrow <bamboogeneral@live.jp>
2019-04-28 03:43:42 +0900

    fix kariru

--- home/views.py
+++ home/views.py
@@ -1,24 +1,28 @@
 from django.shortcuts import render
-
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from .forms import homeForm
-from .models import homeModel 
+from .models import homeModel
 
 
 def index(request):
     return render(request, 'home/index.html')
 
+
 def kasu(request):
     params = {
-            'title': 'home',
-            'message': 'your data:',
-            'form': homeForm()
-        }
+        'form': homeForm(),
+    }
     if (request.method == 'POST'):
-        params['message'] = '名前：' + request.POST['name'] + \
-            '<br>メール：' + request.POST['mail'] + \
-            '<br>年齢：' + request.POST['age']
-        params['form'] = homeForm(request.POST)
+        name = request.POST['name']
+        mail = request.POST['mail']
+        age = request.POST['age']
+        address = request.POST['address']
+        money = request.POST['money']
+        homeSaver = homeModel(name=name, mail=mail,
+                              age=age, address=address, money=money)
+        homeSaver.save()
+        return redirect(to='/kariru')
     return render(request, 'home/kasu.html', params)
 
 
@@ -29,16 +33,17 @@ def kakunin(request):
     address = request.POST['address']
     money = request.POST['money']
     params = {
-        'name':name,
-        'mail':mail,
-        'age':age,
-        'address':address,
-        'money':money,
+        'name': name,
+        'mail': mail,
+        'age': age,
+        'address': address,
+        'money': money,
     }
     return render(request, 'home/kakunin.html')
 
+
 def kariru(request):
-    data = homeModel.objects.all() #レコードを表示する
+    data = homeModel.objects.all()  # レコードを表示する
     # form.save(commit=True)
     params = {
         'data': data,
