9d0844f2c363fcb50223004a40be7f4c278f90f6
ShortArrow <bamboogeneral@live.jp>
2019-05-24 00:19:58 +0900

    make model

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
-from .models import  homeModel
+from .models import homeModel
+from .models import ownerModel
+from .models import roomModel
+from .models import openPatternModel
+from .models import roomGroupModel
+from .models import timeTableModel
+from .models import illegalDayModel
+from .models import reservationModel
+
 
 admin.site.register(homeModel)
+admin.site.register(ownerModel)
+admin.site.register(roomModel)
+admin.site.register(openPatternModel)
+admin.site.register(roomGroupModel)
+admin.site.register(timeTableModel)
+admin.site.register(illegalDayModel)
+admin.site.register(reservationModel)
